34a8a4f4e8af23e0856af34ae3121ab51f457254
Denis Costello <G00396309@atu.ie>
2024-01-09 22:28:59 +0000

    first commit

---
+++ src/main/java/ie/atu/cicd_project_user/CiCdProjectUserApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd_project_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdProjectUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdProjectUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicd_project_user/CiCdProjectUserApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd_project_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdProjectUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
