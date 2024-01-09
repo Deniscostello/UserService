826eded5a34e6514e18756e79405a2b89cb73475
Denis Costello <G00396309@atu.ie>
2024-01-09 22:37:11 +0000

    Created user with name, user id and email.

--- src/main/java/ie/atu/cicd_project_user/User.java
+++ src/main/java/ie/atu/cicd_project_user/User.java
@@ -1,5 +1,8 @@
 package ie.atu.cicd_project_user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,7 +13,14 @@ import org.springframework.data.annotation.Id;
 @NoArgsConstructor
 public class User {
     @Id
-    private int id;
+    private int userId;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min=2, max=50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @Email(message = "Invalid email address")
+    private String email;
 
 
 }
