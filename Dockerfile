9c0a04daf61d4129c7243841246a33e57289de9f
Denis Costello <G00396309@atu.ie>
2024-01-10 18:39:10 +0000

    Added profiles

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ ENV PROFILE=docker
 WORKDIR /app
 COPY target/CiCD_Project_User-0.0.1-SNAPSHOT.jar /app
 EXPOSE 8081
-CMD ["java", "-jar", "CiCD_Project_User-0.0.1-SNAPSHOT.jar"]
+CMD ["java", "-jar", "CiCD_Project_User-0.0.1-SNAPSHOT.jar", "--spring.profiles.active=docker"]
--- src/main/java/ie/atu/cicd_project_user/RegistrationClient.java
+++ src/main/java/ie/atu/cicd_project_user/RegistrationClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name="registration-service", url ="http://springboot-registrationservice:8082")
+@FeignClient(name="registration-service", url = "${feign.url}")
 public interface RegistrationClient {
     @PostMapping("/confirm")
     String SomeDetails(@RequestBody User user);
