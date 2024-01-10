9045384d5da504a6729e684357a4e9137313a7bc
Denis Costello <G00396309@atu.ie>
2024-01-10 13:15:34 +0000

    Added method to send user details

--- src/main/java/ie/atu/cicd_project_user/RegistrationClient.java
+++ src/main/java/ie/atu/cicd_project_user/RegistrationClient.java
@@ -1,4 +1,11 @@
 package ie.atu.cicd_project_user;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name="registration-service", url ="http://localhost:8082")
 public interface RegistrationClient {
+    @PostMapping("/confirm")
+    String SomeDetails(@RequestBody User user);
 }
